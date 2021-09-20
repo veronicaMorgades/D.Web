25fe772cec06edb381a2acd52ebe035d75ea9afb
Veronica Morgades <veronica.morgades@anima.edu.uy>
2021-09-20 16:43:36 -0300

    Tarea APIs
    Tarea APIs

--- APIs/APIs.js
+++ APIs/APIs.js
@@ -1,39 +1,71 @@
-let randomNumber=parseInt(Math.random()*732);
-console.log(randomNumber);
-key=4828587370487275;
-let heroApi = 'https://superheroapi.com/api.php/'+key+'/'+randomNumber;
+let keyCel= '188DA836D4C246858F05D239AB82A216';
+let keyEmail = 'A1A1997ADD334F2A95AC63E26C107E0B';
 
-const getHero = async () =>{
-    const response = await fetch(heroApi);
-    const data = response.json();
-    console.log(data);
-    return data;
 
-};
+const verificarEmail = async () =>{
 
-const randomHero = async () => {
-    const hero = await getHero();
-    console.log(hero);
-    html=(
-            `<div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${hero.image.url}" alt="Card image cap">
-            <div class="card-body">
+
+    direCorreo=document.getElementById('correo').value;
+
+    link='https://api.verimail.io/v3/verify?email='+direCorreo+'&key='+keyEmail;
+
+   
+
+    const response = await fetch(link).then(response => 
+    response.json().then(data => ({
+        data: data,
+        status: response.status
+    })
+).then(res => {
+    console.log(res.status, res.data)
+    let html='';
+        if(res.data.deliverable===true){
+            html=(
+            `<div class="alert alert-primary" role="alert">
+            Correo electrónico introducido válido
             </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">Cras justo odio</li>
-              <li class="list-group-item">Dapibus ac facilisis in</li>
-              <li class="list-group-item">Vestibulum at eros</li>
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>`
-    );
-        
+             `);
+        }else(html="Correo incorrecto");
+    const div= document.querySelector('.resultados2');
     console.log(html);
-    const div = document.querySelector(".coso");
     div.innerHTML=html;
+}));;
+
 };
 
-randomHero();
+
+
+const verificarNumero = async () =>{
+
+    
+    codNumero=document.getElementById('codNumero').value;
+    numero=document.getElementById('numero').value;
+
+    
+   link='https://api.veriphone.io/v2/verify?phone=%2B'+codNumero+'-'+numero+'&key='+keyCel;
+
+   
+
+   const response = await fetch(link).then(response => 
+    response.json().then(data => ({
+        data: data,
+        status: response.status
+    })
+).then(res => {
+    console.log(res.status, res.data)
+    let html='';
+        console.log(res.data.phone_valid);
+        if(res.data.phone_valid===true){
+            html=(
+            `
+            <div class="alert alert-primary" role="alert">
+            Número introducido válido, país: ${res.data.country}, compañía: ${res.data.carrier}
+            </div>
+            `
+            );
+        }else(html='Número incorrecto');
+        const div= document.querySelector('.resultados');
+        console.log(html);
+        div.innerHTML=html;
+}));;;
+};
